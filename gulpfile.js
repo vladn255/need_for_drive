9f462e418ae14daf99944651da35d19db614c44f
vladn255 <nesvitvlad@yandex.ru>
2021-04-19 14:37:36 +0400

    initial_config

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+// Styles
+
+const styles = () => {
+  return gulp.src("src/styles/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("public/css"))
+}
+
+exports.styles = styles;
